9607e002e2b46e2483946b64589e401d5d38d51e
Josedageo <jose.geo.aqua@gmail.com>
2024-05-24 14:04:31 +0200

    Update Plot_AAS.py

--- Plot_AAS.py
+++ Plot_AAS.py
@@ -34,30 +34,47 @@ if st.sidebar.checkbox("Show Data Info"):
     st.write("Columns: ", control_points_df.columns.tolist())
 
 # Sidebar for selecting plot parameters
+file_selection = st.sidebar.selectbox('Select Data File', ['Hydrographs', 'Kvalues'])
 plot_type = st.sidebar.selectbox('Select Plot Type', [
     'scatter', 'line', 'bar', 'area', 'pie', 'histogram', 'box', 
     'violin', 'surface', 'heatmap'
 ])
-x_axis = st.sidebar.selectbox('Select X Axis', hydrographs_df.columns)
-y_axis = st.sidebar.selectbox('Select Y Axis', hydrographs_df.columns)
-z_axis = st.sidebar.selectbox('Select Z Axis (if applicable)', [None] + list(hydrographs_df.columns))
+
+if file_selection == 'Hydrographs':
+    df = hydrographs_df
+    x_axis = st.sidebar.selectbox('Select X Axis', df.columns)
+    y_axis = st.sidebar.selectbox('Select Y Axis', df.columns)
+    z_axis = st.sidebar.selectbox('Select Z Axis (if applicable)', [None] + list(df.columns))
+else:
+    df = kvalues_df
+    x_axis = st.sidebar.selectbox('Select X Axis', df.columns)
+    y_axis = st.sidebar.selectbox('Select Y Axis', df.columns)
+    z_axis = st.sidebar.selectbox('Select Z Axis (if applicable)', [None] + list(df.columns))
 
 # Filtering options
-filter_columns = st.sidebar.multiselect('Filter Columns', hydrographs_df.columns)
+filter_columns = st.sidebar.multiselect('Filter Columns', df.columns)
 filters = {}
 for column in filter_columns:
-    unique_values = hydrographs_df[column].unique()
+    unique_values = df[column].unique()
     selected_values = st.sidebar.multiselect(f'Select values for {column}', unique_values, default=unique_values)
     filters[column] = selected_values
 
 # Apply filters
-filtered_df = hydrographs_df.copy()
+filtered_df = df.copy()
 for column, values in filters.items():
     filtered_df = filtered_df[filtered_df[column].isin(values)]
 
-# Lookup and merge additional data dynamically
-# Adding columns from control_points_df and kvalues_df as needed for the plot
-if 'Label_unique' in filtered_df.columns:
+# Percentile filter
+percentile_filter = st.sidebar.checkbox("Filter by Percentile")
+if percentile_filter:
+    percentile_value = st.sidebar.slider('Select Percentile Range', 0, 100, (5, 95))
+    if y_axis in filtered_df.columns:
+        lower_bound = filtered_df[y_axis].quantile(percentile_value[0] / 100)
+        upper_bound = filtered_df[y_axis].quantile(percentile_value[1] / 100)
+        filtered_df = filtered_df[(filtered_df[y_axis] >= lower_bound) & (filtered_df[y_axis] <= upper_bound)]
+
+# Lookup and merge additional data dynamically for Hydrographs file
+if file_selection == 'Hydrographs' and 'Label_unique' in filtered_df.columns:
     filtered_df = filtered_df.merge(control_points_df[['ID', 'Zone', 'Level']], left_on='Label_unique', right_on='ID', how='left')
     filtered_df = filtered_df.merge(kvalues_df, on=['Level', 'Zone'], how='left')
 
@@ -144,3 +161,19 @@ if fig:
     st.plotly_chart(fig)
 else:
     st.write('Select a valid plot type and axes.')
+
+# Additional plotting options for Kvalues and Control Points
+st.sidebar.header('Additional Plots')
+plot_additional = st.sidebar.checkbox('Plot Additional Data')
+if plot_additional:
+    additional_x_axis = st.sidebar.selectbox('Select X Axis for Additional Plot', kvalues_df.columns)
+    additional_y_axis = st.sidebar.selectbox('Select Y Axis for Additional Plot', kvalues_df.columns)
+    additional_df = kvalues_df.copy()
+    # Apply filters based on zone and level from control_points_df
+    if 'Zone' in control_points_df.columns and 'Level' in control_points_df.columns:
+        selected_zone = st.sidebar.selectbox('Select Zone', control_points_df['Zone'].unique())
+        selected_level = st.sidebar.selectbox('Select Level', control_points_df['Level'].unique())
+        additional_df = additional_df[(additional_df['Zone'] == selected_zone) & (additional_df['Level'] == selected_level)]
+
+    additional_fig = px.scatter(additional_df, x=additional_x_axis, y=additional_y_axis)
+    st.plotly_chart(additional_fig)
